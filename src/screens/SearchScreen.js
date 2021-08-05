8cae6d6836efd47e6f5e1f637fc66af09d4795da
Onny Vergiea Saputra <onny.s@students.amikom.ac.id>
2021-08-05 21:26:38 +0700

    Add yelp API using AXIOS

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,36 @@
 import React,{ useState } from 'react';
 import { View,Text,StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
     const [term , setTerm] = useState('')
+    const [result, setResult] = useState([])
+
+    const searchApi = async () => {
+    try{
+        const response = await yelp.get('/search',{
+            params:{
+                limit: 50,
+                term,
+                location: 'san jose'
+            }
+        })
+            setResult(response.data.businesses)
+        }catch(err){
+            console.log(err)
+        }  
+    }
+
     return (
     <View>
         <SearchBar 
             term={term} 
-            onTermChange={newTerm => setTerm(newTerm)}
-            onTermSubmit={() => console.log('term was submitted')}
+            onTermChange={setTerm}
+            onTermSubmit={searchApi}
         />
         <Text>Search Screen</Text>
+        <Text>We have found {result.length} result</Text>
     </View>
     )
 }
