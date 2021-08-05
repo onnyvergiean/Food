9dc2e49c44222246bcf80f4c0ae522ce625186b6
Onny Vergiea Saputra <onny.s@students.amikom.ac.id>
2021-08-05 22:38:23 +0700

    Add useEffect for default item

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,19 @@
-import React,{ useState } from 'react';
+import React,{ useState, useEffect } from 'react';
 import { View,Text,StyleSheet } from 'react-native';
 import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 
-const SearchScreen = () => {
+const SearchScreen = (searchTerm) => {
     const [term , setTerm] = useState('')
     const [result, setResult] = useState([])
     const [errorMessage, setErrorMessage] = useState('')
 
-    const searchApi = async () => {
+    const searchApi = async (searchTerm) => {
     try{
         const response = await yelp.get('/search',{
             params:{
                 limit: 50,
-                term,
+                term : searchTerm,
                 location: 'san jose'
             }
         })
@@ -23,12 +23,16 @@ const SearchScreen = () => {
         }  
     }
 
+    useEffect(()=> {
+        searchApi('pasta')
+    }, [])
+
     return (
     <View>
         <SearchBar 
             term={term} 
             onTermChange={setTerm}
-            onTermSubmit={searchApi}
+            onTermSubmit={() => searchApi(term)}
         />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {result.length} result</Text>
