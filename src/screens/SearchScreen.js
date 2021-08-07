b2a2789ecfa79e3302f1034031b78deef8075155
Onny Vergiea Saputra <onny.s@students.amikom.ac.id>
2021-08-07 14:59:49 +0700

    Add Resuable Components ResultList

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,8 +1,9 @@
-import React,{ useState, useEffect } from 'react';
+import React,{ useState } from 'react';
 import { View,Text,StyleSheet } from 'react-native';
 import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 import useResult from '../hooks/useResult';
+import ResultList from '../components/ResultList';
 
 const SearchScreen = (searchTerm) => {
     const [term , setTerm] = useState('')
@@ -17,6 +18,9 @@ const SearchScreen = (searchTerm) => {
         />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
         <Text>We have found {result.length} result</Text>
+        <ResultList title='Cost Effective'/>
+        <ResultList title='Bit Pricier'/>
+        <ResultList title='Big Spender'/>
     </View>
     )
 }
